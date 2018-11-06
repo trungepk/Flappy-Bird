9976c06adfc44982f47903116dc8b87e3cd96811
trungepk <nevergiveup14896@gmail.com>
2018-11-06 18:13:59 +0700

    Add bird controller

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bird : MonoBehaviour {
+    [SerializeField] float upForce = 200f;
+    private bool isDead;
+    private Rigidbody2D rb2d;
+
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+}
