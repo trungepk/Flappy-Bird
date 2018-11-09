c4f346a2987a5e89ad93789ff1606ce23a700dac
trungepk <nevergiveup14896@gmail.com>
2018-11-09 23:36:04 +0700

    Column Obstacles

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
     public static GameControl instance;
     [SerializeField] GameObject gameOverText;
+    [SerializeField] Text scoreText;
     public bool gameOver;
+
+    private int score;
 
 	void Awake () {
         if (instance == null)
@@ -32,4 +36,10 @@ public class GameControl : MonoBehaviour {
         gameOver = true;
     }
 	
+    public void Score()
+    {
+        if (gameOver) { return; }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
 }
--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class ScrollingObject : MonoBehaviour {
     [SerializeField] float scrollSpeed = -1f;
 
